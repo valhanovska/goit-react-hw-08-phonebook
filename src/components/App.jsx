40462a0043a3977577b096b86441d7cde920b071
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-10-08 22:53:45 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,52 @@
-import Form from '../components/Form';
-import ContactList from '../components/ContactList';
-import Filter from '../components/Filter';
+import { Route, Routes, NavLink } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import s from './App.module.css';
+import { lazy, Suspense } from 'react';
+// import PrivateRoute from 'pages/PrivateRoute';
+// import ContactList from './ContactList';
 
-export function App() {
+// const MyComponent = lazy(() => import('path/to/MyComponent'));
+
+const Register = lazy(() => import('../pages/Register'));
+const Login = lazy(() => import('../pages/Login'));
+const Contacts = lazy(() => import('./Contacts'));
+const PrivateRoute = lazy(() => import('pages/PrivateRoute'));
+
+// const Cast = lazy(() => import('../pages/Cast'));
+// const Reviews = lazy(() => import('../pages/Reviews'));
+
+export const App = () => {
   return (
-    <div className={s.container}>
-      <div>
-        <h1>Phonebook</h1>
-        <Form />
-      </div>
-      <div>
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
-      </div>
-    </div>
+    <>
+      <nav className={s.nav}>
+        <NavLink to="/login" end className={s.link}>
+          Login
+        </NavLink>
+        <NavLink to="/register" className={s.link}>
+          Register
+        </NavLink>
+      </nav>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route element={<PrivateRoute />}>
+            <Route path="/contacts" element={<Contacts />}></Route>
+          </Route>
+          {/* <Route path="/contacts" element={<Contacts />}> */}
+          {/* <Route path="cast" element={<Cast />} /> */}
+          {/* <Route path="reviews" element={<Reviews />} /> */}
+          {/* </Route> */}
+
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </Suspense>
+
+      <ToastContainer autoClose={3000} />
+    </>
   );
-}
+};
