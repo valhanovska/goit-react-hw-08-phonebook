620c06012c898430db495d803575064137f4f43c
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-10-13 17:10:47 +0300

    Delete selectors.js

--- src/redux/selectors.js
+++
@@ -1,5 +0,0 @@
-export const getContacts = state => state.contacts.items;
-export const getFilter = state => state.filter;
-export const getLoading = state => state.contacts.isLoading;
-
-// state.contacts.contacts.items;
