c5527baad4e5433bed7c81e7ddb770267e6e1315
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-10-13 17:10:31 +0300

    Create selectors.js

---
+++ src/redux/Contacts/selectors.js
@@ -0,0 +1,4 @@
+export const getContacts = state => state.contacts.items;
+export const getFilter = state => state.filter;
+export const getLoading = state => state.contacts.isLoading;
+export const getEditContact = state => state.contacts.editContact;
