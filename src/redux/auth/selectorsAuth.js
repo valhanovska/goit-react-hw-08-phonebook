0ef8cee2f455cbba0c0aabb108f70e617297b0b6
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-10-13 17:10:19 +0300

    Create selectorsAuth.js

---
+++ src/redux/auth/selectorsAuth.js
@@ -0,0 +1,4 @@
+export const getUserName = state => state.auth.user.name;
+export const getIsLoggedIn = state => state.auth.isLoggedIn;
+
+export const getIsRefreshing = state => state.auth.isRefreshing;
