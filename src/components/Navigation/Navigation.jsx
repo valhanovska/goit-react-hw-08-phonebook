a117d1db65862c5fe54c58015aaeb8c346a6f897
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-10-14 11:46:59 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,12 +3,14 @@ import { useSelector } from 'react-redux';
 import { getIsLoggedIn } from 'redux/auth/selectorsAuth';
 
 import s from './Navigation.module.css';
+import ColorMode from 'components/ColorMode';
 
 export const Navigation = () => {
   const isLoggedIn = useSelector(getIsLoggedIn);
 
   return (
     <nav>
+      <ColorMode />
       <NavLink className={s.link} to="/">
         Home
       </NavLink>
